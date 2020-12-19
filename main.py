522696b5304022689a95b8646fe4399d72896d45
Mykola Riabchenko <mriabchenko11@gmail.com>
2020-12-19 23:14:23 +0200

    added CORS

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 
 from app.order.endpoinds import order
 from app.user.endpoints import login, user
@@ -7,6 +9,15 @@ from config import settings
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
 
 app.include_router(login.router, prefix=settings.API_V1_STR + "/user", tags=["login"])
 app.include_router(user.router, prefix=settings.API_V1_STR + "/user", tags=["user"])
